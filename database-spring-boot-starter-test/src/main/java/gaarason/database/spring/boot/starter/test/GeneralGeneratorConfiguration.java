7aaba40f6da0c052120dec598a640cf2ad609d04
xut05 <xut05@mingyuanyun.com>
2020-01-07 11:30:55 +0800

    GeneralGeneratorConfiguration fix

--- database-spring-boot-starter-test/src/main/java/gaarason/database/spring/boot/starter/test/GeneralGeneratorConfiguration.java
+++ database-spring-boot-starter-test/src/main/java/gaarason/database/spring/boot/starter/test/GeneralGeneratorConfiguration.java
@@ -1,17 +1,13 @@
 package gaarason.database.spring.boot.starter.test;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import javax.sql.DataSource;
 
 @Configuration
-@ConditionalOnBean(DataSource.class)
+@AutoConfigureAfter(DataSource.class)
 @Import(GeneralGenerator.class)
 public class GeneralGeneratorConfiguration {
 
