93163d531a7485559c1151158ef22874ee1b14db
xutengx <68822684@qq.com>
2021-09-30 14:29:59 +0800

    更名 DBShadowProvider -> DatabaseShadowProvider

--- database-core/src/main/java/gaarason/database/provider/DatabaseShadowProvider.java
+++ database-core/src/main/java/gaarason/database/provider/DatabaseShadowProvider.java
@@ -12,33 +12,33 @@ import java.util.concurrent.ConcurrentHashMap;
  * 数据库信息大全
  * @author xt
  */
-public final class DBShadowProvider {
+public final class DatabaseShadowProvider {
 
     /**
      * 表信息
      */
-    private final static Map<GaarasonDataSource, ConcurrentHashMap<String, LinkedHashMap<String, DBColumn>>> info = new ConcurrentHashMap<>();
+    private static final Map<GaarasonDataSource, ConcurrentHashMap<String, LinkedHashMap<String, DBColumn>>> INFO = new ConcurrentHashMap<>();
 
-    private DBShadowProvider(){
+    private DatabaseShadowProvider() {
 
     }
 
     /**
      * 获取表的信息
      * @param gaarasonDataSource 数据源
-     * @param table 表名
+     * @param table              表名
      * @return 表信息
      */
-    public static LinkedHashMap<String, DBColumn> getTable(GaarasonDataSource gaarasonDataSource, String table) {
-        ConcurrentHashMap<String, LinkedHashMap<String, DBColumn>> manyTableInfoMap = info.get(gaarasonDataSource);
+    public static Map<String, DBColumn> getTable(GaarasonDataSource gaarasonDataSource, String table) {
+        ConcurrentHashMap<String, LinkedHashMap<String, DBColumn>> manyTableInfoMap = INFO.get(gaarasonDataSource);
         if (manyTableInfoMap == null) {
             synchronized (gaarasonDataSource) {
-                manyTableInfoMap = info.get(gaarasonDataSource);
+                manyTableInfoMap = INFO.get(gaarasonDataSource);
                 if (manyTableInfoMap == null) {
                     // 获取表信息
-                    manyTableInfoMap = new ConcurrentHashMap<>();
+                    manyTableInfoMap = new ConcurrentHashMap<>(16);
                     // 进入缓存
-                    info.put(gaarasonDataSource, manyTableInfoMap);
+                    INFO.put(gaarasonDataSource, manyTableInfoMap);
                 }
             }
         }
@@ -48,11 +48,11 @@ public final class DBShadowProvider {
     /**
      * 获取表的信息
      * @param gaarasonDataSource 数据源
-     * @param manyTableInfoMap 许多的表信息
-     * @param table 表名
+     * @param manyTableInfoMap   许多的表信息
+     * @param table              表名
      * @return 表信息
      */
-    private static LinkedHashMap<String, DBColumn> getTableFromGaarasonDataSource(GaarasonDataSource gaarasonDataSource,
+    private static Map<String, DBColumn> getTableFromGaarasonDataSource(GaarasonDataSource gaarasonDataSource,
         ConcurrentHashMap<String, LinkedHashMap<String, DBColumn>> manyTableInfoMap, String table) {
         LinkedHashMap<String, DBColumn> tableInfoMap = manyTableInfoMap.get(table);
         if (tableInfoMap == null) {
